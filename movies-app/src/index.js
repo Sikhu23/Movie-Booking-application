786ca417198a0ed81ef573c2a556cba561c1234b
Sikhu <its.siddharth2308@gmail.com>
2021-09-13 20:41:12 +0530

    Checkpoint 3- Added Heading and Home Component

--- movies-app/src/index.js
+++ movies-app/src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
-import Header from './common/header/Header'
+import Home from './screens/home/Home';
 
 ReactDOM.render(
   
-    <Header/>
+    <Home/>
     
     
   ,
---
+++ movies-app/src/screens/home/Home.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import Header from './../../common/header/Header';
+import './Home.css';
+
+export default class Home extends Component{
+    render(){
+
+        return(
+
+            <div>
+                <Header/>
+                <span className="heading">Upcoming Movies</span>
+            </div>
+
+
+
+        )
+
+
+
+
+    }
+}
