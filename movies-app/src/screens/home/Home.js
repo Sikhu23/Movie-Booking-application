421c8e3655a96bb77735d82227040a1353bdec0a
Sikhu <its.siddharth2308@gmail.com>
2021-09-14 20:00:02 +0530

    Checkpoint 3 - Added ImageList MaterialUI with required details

--- movies-app/src/screens/home/Home.js
+++ movies-app/src/screens/home/Home.js
@@ -1,6 +1,10 @@
 import { Component } from "react";
 import Header from './../../common/header/Header';
 import './Home.css';
+import moviesData from "../../common/moviesData/moviesData";
+import { ImageListItem } from "@material-ui/core";
+import { ImageListItemBar } from "@material-ui/core";
+import SingleLineImageList from '../../common/header/ImageList/ImageList'
 
 export default class Home extends Component{
     render(){
@@ -10,6 +14,8 @@ export default class Home extends Component{
             <div>
                 <Header/>
                 <span className="heading">Upcoming Movies</span>
+                
+                <SingleLineImageList itemData={moviesData}></SingleLineImageList>
             </div>
 
 
